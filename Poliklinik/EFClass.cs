a22dcb4b2385525ac18090389a93ef9474600118
Иван Котлов <garri_gun@bk.ru>
2023-06-01 11:33:24 +0300

    Добавьте файлы проекта.

---
+++ Poliklinik/EFClass.cs
@@ -0,0 +1,16 @@
+﻿using Poliklinik.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Poliklinik
+{
+    public class EFClass
+    {
+        public static Entities entities = new Entities();
+    }
+}
---
+++ Poliklinik/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Poliklinik.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Poliklinik
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            get();
+        }
+
+        public void get()
+        {
+            List<Add> list = new List<Add>();
+            LvKlinik.ItemsSource = EFClass.entities.Add.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
